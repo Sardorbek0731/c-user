d76d5ef05d2e5742fcc28ada62fdb412ac7dd954
Sardorbek Olimov <sardor.20070731@gmail.com>
2023-12-04 19:12:16 +0500

    shorted code

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import "./App.css";
 import { useState } from "react";
 
 // JSX files
-import Header from "./components/navbar/Header.jsx";
-import ShowUsers from "./components/showUser/showUsers.jsx";
-import Footer from "./components/footer/Footer.jsx";
-import Modal from "./components/modal/Modal.jsx";
+import Header from "./components/navbar/Header";
+import ShowUsers from "./components/showUser/showUsers";
+import Footer from "./components/footer/Footer";
+import Modal from "./components/modal/Modal";
 
 function App() {
   const [showModal, setShowModal] = useState(false);
